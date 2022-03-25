3855d0daf2331fb088e99a1fc340e5d1cfff21cc
CollinPArnold <89942059+CollinPArnold@users.noreply.github.com>
2022-03-24 23:26:38 -0500

    Submitted - 11:30

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,5 +1,7 @@
 package labs;
 
+import java.util.Scanner;
+import java.lang.Math;
 
 
 public class Lab01 {
@@ -16,7 +18,16 @@ public class Lab01 {
 
 	
 	public static void problem01() {
+		Scanner p1 = new Scanner(System.in);
+		System.out.print("Enter a positive integer : ");
+		int input1 = p1.nextInt();
+		System.out.print("Enter an exponent : ");
+		int input2 = p1.nextInt();
 		
+		int mth = (int) Math.pow(input1, input2);
+		
+		System.out.println(input1 + "^" + input2 + " = " + mth);
+		System.out.println("");
 		
 		
 	}
@@ -25,8 +36,13 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
+		Scanner p2 = new Scanner(System.in);
+		System.out.print("Enter a positive integer : ");
+		int input = p2.nextInt();
+		double sqr = Math.pow(input, 0.5);
 		
-		
+		System.out.println("The square root of " + input + " is " + sqr);
+		System.out.println("");
 		
 	}
 
@@ -34,9 +50,21 @@ public class Lab01 {
 	
 
 	public static void problem03() {
+		Scanner p3 = new Scanner(System.in);
+		System.out.print("Length of side A : ");
+		double sideA = p3.nextDouble();
 		
+		System.out.print("Length of side B : ");
+		double sideB = p3.nextDouble();
 		
+		double sqA = sideA * sideA;
+		double sqB = sideB * sideB;
 		
+		double hypoSq = sqA + sqB;
+		double hypo = Math.pow(hypoSq, 0.5);
+		
+		System.out.println("Hypothenuse = " + hypo);
+		System.out.println("");
 
 	}
 
@@ -44,8 +72,25 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		Scanner p4 = new Scanner(System.in);
+		int input = 1;
+		int max = 0; int min = 0;
 		
-		
+		while (input != 0) {
+			System.out.print("Enter a positive integer : ");
+			input = p4.nextInt();
+			if (input == 0) {
+				break;
+			}
+			if (input >= max) {
+				max = input;
+			} else if (input <= min) {
+				min = input;
+			}
+			
+		}
+		System.out.println("Max = " + max);
+		System.out.println("Min = " + min);
 		
 	}
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,14 +1,16 @@
 package labs;
 
+import java.util.Scanner;
+import java.lang.Math;
 
 
 public class Lab02 {
 
 	public static void main(String[] args) {
 		
-		problem01();
-		problem02();
-		problem03();
+		//problem01();
+		//problem02();
+		//problem03();
 		problem04();
 		
 	}
@@ -16,9 +18,28 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		Scanner p2 = new Scanner(System.in);
+		System.out.print("Enter a positive integer : ");
+		int max = p2.nextInt();
+		System.out.print("Enter another positive integer : ");
+		int min = p2.nextInt();
+		int t = 0;
 		
 		
+		for (int i = 1; i < 10; i++) {
+			if (max > min) {
+				t =  min + (int)(Math.random() * ((max - min) + 1));
+			} else if (min > max) {
+				t =  max + (int)(Math.random() * ((min - max) + 1));
+			} else if (max == min) {
+				System.out.println("Hey wait... Those numbers are equal");
+				break;
+			}
+			
+			System.out.print(t +  " ");
+		}
 		
+		System.out.println("");
 		
 	}
 	
@@ -26,8 +47,18 @@ public class Lab02 {
 	
 	
 	public static void problem02() {
+		Scanner p3 = new Scanner(System.in);
+		System.out.print("Enter the radius of the cylinder : ");
+		double radius = p3.nextInt();
+		double circle = radius * radius * Math. PI;
 		
+		System.out.print("Enter the height of the cylinder : ");
+		double height = p3.nextInt();
 		
+		double cyl = circle * height;
+		
+		System.out.println(cyl);
+		System.out.println("");
 		
 	}
 
@@ -35,7 +66,29 @@ public class Lab02 {
 	
 
 	public static void problem03() {
-
+		Scanner p4 = new Scanner(System.in);
+		System.out.print("Enter x1 : ");
+		double x1 = p4.nextDouble();
+		System.out.print("Enter y1 : ");
+		double y1 = p4.nextDouble();
+		
+		System.out.print("Enter x2 : ");
+		int x2 = p4.nextInt();
+		System.out.print("Enter y2 : ");
+		int y2 = p4.nextInt();
+		
+		double x = x2 - x1;
+		double xsq = Math.pow(x, 2);
+		
+		double y = y2 - y1;
+		double ysq = Math.pow(y, 2);
+		
+		double xy = xsq + ysq;
+		
+		double d = Math.pow(xy, 0.5);
+		
+		System.out.println("Distance between point " + d);
+		
 		
 		
 	}
@@ -44,9 +97,36 @@ public class Lab02 {
 	
 	
 	public static void problem04() {
+		//Ax^2 + Bx + C = 0
 		
+		Scanner p5 = new Scanner(System.in);
+		System.out.print("Enter a-value : ");
+		double a = p5.nextDouble();
+		System.out.print("Enter b-value : ");
+		double b = p5.nextDouble();
+		System.out.print("Enter c-value : ");
+		double c = p5.nextDouble();
 		
+		double NB = b * -1;
 		
+		double b2 = b * b;
+		double fourAC = 4 * a * c;
+		double inside = b2 - fourAC;
+		double sr = Math.pow(inside, 0.5);
+		double top = NB + sr;
+		double bottom = 2 * a;
+		double all = top / bottom;
+		
+		System.out.println("x1 = " + all);
+		
+		b2 = b * b;
+		fourAC = 4 * a * c;
+		inside = b2 - fourAC;
+		sr = Math.pow(inside, 0.5);
+		top = NB - sr;
+		bottom = 2 * a;
+		all = top / bottom;
+		System.out.println("x2 = " + all);
 		
 	}
 	
